e0ca6ec2ff4db6c3bc53868a80ae35b6fa720c01
Jimil Digaswala <92706888+Jimil1407@users.noreply.github.com>
2025-08-02 18:43:09 +0530

    added middleware

---
+++ middlewares/admin.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+const secretKey = process.env.SECRET_KEY;
+
+const adminMiddleware = (req, res, next) => {
+  const token = req.headers.token;
+  const decoded = jwt.verify(token, secretKey);
+  if (!decoded) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }else{
+    req.admin_id = decoded.id;
+    next();
+  }
+};
+
+export default adminMiddleware;
